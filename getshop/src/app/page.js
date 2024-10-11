0f1cdd30dffbdbe2bf7ef7440abf38c08f4434b4
Rustam <tabarzin@gmail.com>
2024-10-11 20:32:27 +0300

    feat: add logos

--- getshop/src/app/page.js
+++ getshop/src/app/page.js
@@ -5,6 +5,7 @@ import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import TabsBlock from "@/components/TabsBlock/TabsBlock";
 import Balance from "../components/Balance/Balance";
+import Partners from "../components/Partners/Partners";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
             <TabsBlock />
           </div>
           <Balance />
+          <Partners />
         </div>
       </main>
       <footer className={styles.footer}>footer</footer>
---
+++ getshop/src/components/Partners/Partners.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "./Partners.module.css";
+import clickwave from "../../assets/images/clickwave.png";
+import nova from "../../assets/images/novasphere.png";
+import zy from "../../assets/images/zypth.png";
+
+const Partners = () => {
+  return (
+    <section className={styles.partners}>
+      <h2 className={styles.partners_title}>Наши партнёры по монетизации</h2>
+      <div className={styles.logos}>
+        <Image src={clickwave} alt="Clickwave Logo" width={177} height={54} />
+        <Image src={zy} alt="Zypth Logo" width={102} height={70} />
+        <Image src={nova} alt="Novasphere Logo" width={191} height={60} />
+      </div>
+    </section>
+  );
+};
+
+export default Partners;
